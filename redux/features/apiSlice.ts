bb496b7f7902552c3715e65b76357b3651d188e0
Mekin-jema <mekinjemal999@gmail.com>
2026-02-03 20:25:47 +0300

    refactor: update API base URL for production environment

--- redux/features/apiSlice.ts
+++ redux/features/apiSlice.ts
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl:'http://localhost:8000/api', 
-    // baseUrl: 'https://eic-backend-9heh.onrender.com/api',
+    // baseUrl:'http://localhost:8000/api', 
+    baseUrl: 'https://eic-main-backend-2.onrender.com/api',
     credentials: 'include',
 })
 
